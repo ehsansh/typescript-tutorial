8f0c2f0554fb7c21f743e56794d26efbf9e33ade
Ehsan Shadmehri <ehsanshadmehri@gmail.com>
2023-05-21 15:36:52 +0330

    create an invoice class

--- public/app.js
+++ public/app.js
@@ -8,3 +8,18 @@ form.addEventListener('submit', (e) => {
     e.preventDefault();
     console.log(type.value, amount.valueAsNumber);
 });
+class Invoice {
+    constructor(c, d, a) {
+        this.client = c;
+        this.details = d;
+        this.amount = a;
+    }
+    format() {
+        return `${this.client} owes ${this.amount} for ${this.details}`;
+    }
+}
+const inv1 = new Invoice('ehsan', 'work on website', 300);
+const inv2 = new Invoice('james', 'work on website', 200);
+let invoices = [];
+invoices.push(inv1);
+inv1.client = 'henry';
--- src/app.ts
+++ src/app.ts
@@ -10,3 +10,26 @@ form.addEventListener('submit', (e: Event) => {
     e.preventDefault();
     console.log(type.value, amount.valueAsNumber);
 });
+
+class Invoice {
+    client: string;
+    details: string;
+    amount: number;
+    constructor(c: string, d: string, a: number) {
+        this.client = c;
+        this.details = d;
+        this.amount = a;
+    }
+    format() {
+        return `${this.client} owes ${this.amount} for ${this.details}`;
+    }
+}
+
+const inv1 = new Invoice('ehsan', 'work on website', 300);
+const inv2 = new Invoice('james', 'work on website', 200);
+
+let invoices: Invoice[] = [];
+
+invoices.push(inv1);
+
+inv1.client = 'henry';
