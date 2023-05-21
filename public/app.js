6d5744207263f5c85c39d2ff5f1df7724f55d3d0
Ehsan Shadmehri <ehsanshadmehri@gmail.com>
2023-05-21 20:09:43 +0330

    reformat constructor

--- public/app.js
+++ public/app.js
@@ -9,10 +9,13 @@ form.addEventListener('submit', (e) => {
     console.log(type.value, amount.valueAsNumber);
 });
 class Invoice {
-    constructor(c, d, a) {
-        this.client = c;
-        this.details = d;
-        this.amount = a;
+    constructor(client, details, amount) {
+        this.client = client;
+        this.details = details;
+        this.amount = amount;
+        // this.client = c;
+        // this.details = d;
+        // this.amount = a;
     }
     format() {
         return `${this.client} owes ${this.amount} for ${this.details}`;
--- src/app.ts
+++ src/app.ts
@@ -12,13 +12,14 @@ form.addEventListener('submit', (e: Event) => {
 });
 
 class Invoice {
-    readonly client: string;
-    private details: string;
-    public amount: number;
-    constructor(c: string, d: string, a: number) {
-        this.client = c;
-        this.details = d;
-        this.amount = a;
+    constructor(
+        readonly client: string,
+        private details: string,
+        public amount: number
+    ) {
+        // this.client = c;
+        // this.details = d;
+        // this.amount = a;
     }
     format() {
         return `${this.client} owes ${this.amount} for ${this.details}`;
