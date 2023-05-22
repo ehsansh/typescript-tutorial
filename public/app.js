be7cf1a58e2d6621d3ed39c2d8a4f40d1000cedc
Ehsan Shadmehri <ehsanshadmehri@gmail.com>
2023-05-22 12:14:01 +0330

    use tuples in form submission

--- public/app.js
+++ public/app.js
@@ -11,11 +11,16 @@ const list = new ListTemplate(ul);
 form.addEventListener('submit', (e) => {
     e.preventDefault();
     let doc;
+    let values = [
+        tofrom.value,
+        details.value,
+        amount.valueAsNumber,
+    ];
     if (type.value === 'invoice') {
-        doc = new Invoice(tofrom.value, details.value, amount.valueAsNumber);
+        doc = new Invoice(...values);
     }
     else {
-        doc = new Payment(tofrom.value, details.value, amount.valueAsNumber);
+        doc = new Payment(...values);
     }
     list.render(doc, type.value, 'end');
 });
--- src/app.ts
+++ src/app.ts
@@ -18,10 +18,16 @@ form.addEventListener('submit', (e: Event) => {
     e.preventDefault();
     let doc: HasFormatter;
 
+    let values: [string, string, number] = [
+        tofrom.value,
+        details.value,
+        amount.valueAsNumber,
+    ];
+
     if (type.value === 'invoice') {
-        doc = new Invoice(tofrom.value, details.value, amount.valueAsNumber);
+        doc = new Invoice(...values);
     } else {
-        doc = new Payment(tofrom.value, details.value, amount.valueAsNumber);
+        doc = new Payment(...values);
     }
 
     list.render(doc, type.value, 'end');
