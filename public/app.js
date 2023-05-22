5ddb84092d8850eace0ee7b2823efd8a6c57e5a5
Ehsan Shadmehri <ehsanshadmehri@gmail.com>
2023-05-22 11:02:09 +0330

    use Invoice and Payment classes after the form is sumitted

--- public/app.js
+++ public/app.js
@@ -1,4 +1,5 @@
 import { Invoice } from './classes/Invoice.js';
+import { Payment } from './classes/Payment.js';
 const form = document.querySelector('.new-item-form');
 const type = document.querySelector('#type');
 const tofrom = document.querySelector('#tofrom');
@@ -6,30 +7,49 @@ const details = document.querySelector('#details');
 const amount = document.querySelector('#amount');
 form.addEventListener('submit', (e) => {
     e.preventDefault();
-    console.log(type.value, amount.valueAsNumber);
+    let doc;
+    if (type.value === 'invoice') {
+        doc = new Invoice(tofrom.value, details.value, amount.valueAsNumber);
+    }
+    else {
+        doc = new Payment(tofrom.value, details.value, amount.valueAsNumber);
+    }
+    console.log(doc);
 });
-const inv1 = new Invoice('ehsan', 'work on website', 300);
-const inv2 = new Invoice('james', 'work on website', 200);
-let invoices = [];
-invoices.push(inv1);
-invoices.push(inv2);
-console.log('here');
-invoices.forEach(inv => {
-    console.log(inv.client, inv.amount, inv.format());
-});
-const me = {
-    name: 'John',
-    age: 23,
-    speak(text) {
-        console.log(text);
-    },
-    spend(amount) {
-        console.log('I spent', amount);
-        return amount;
-    },
-};
-console.log(me);
-const greeting = (person) => {
-    console.log(person.name);
-};
-greeting(me);
+// let docOne: HasFormatter;
+// let docTwo: HasFormatter;
+// docOne = new Invoice('ehsan', 'web work', 250);
+// docTwo = new Invoice('james', 'teaching work', 200);
+// let docs: HasFormatter[] = [];
+// docs.push(docOne);
+// docs.push(docTwo);
+// const inv1 = new Invoice('ehsan', 'work on website', 300);
+// const inv2 = new Invoice('james', 'work on website', 200);
+// let invoices: Invoice[] = [];
+// invoices.push(inv1);
+// invoices.push(inv2);
+// invoices.forEach(inv => {
+//     console.log(inv.client, inv.amount, inv.format());
+// });
+// interface IsPerson {
+//     name: string;
+//     age: number;
+//     speak(a: string): void;
+//     spend(a: number): number;
+// }
+// const me: IsPerson = {
+//     name: 'John',
+//     age: 23,
+//     speak(text: string): void {
+//         console.log(text);
+//     },
+//     spend(amount: number): number {
+//         console.log('I spent', amount);
+//         return amount;
+//     },
+// };
+// console.log(me);
+// const greeting = (person: IsPerson) => {
+//     console.log(person.name);
+// };
+// greeting(me);
--- src/app.ts
+++ src/app.ts
@@ -2,16 +2,6 @@ import { Invoice } from './classes/Invoice.js';
 import { Payment } from './classes/Payment.js';
 import { HasFormatter } from './interfaces/HasFormatter.js';
 
-let docOne: HasFormatter;
-let docTwo: HasFormatter;
-
-docOne = new Invoice('ehsan', 'web work', 250);
-docTwo = new Invoice('james', 'teaching work', 200);
-
-let docs: HasFormatter[] = [];
-docs.push(docOne);
-docs.push(docTwo);
-
 const form = document.querySelector('.new-item-form') as HTMLFormElement;
 
 const type = document.querySelector('#type') as HTMLSelectElement;
@@ -22,44 +12,62 @@ const amount = document.querySelector('#amount') as HTMLInputElement;
 
 form.addEventListener('submit', (e: Event) => {
     e.preventDefault();
-    console.log(type.value, amount.valueAsNumber);
+    let doc: HasFormatter;
+
+    if (type.value === 'invoice') {
+        doc = new Invoice(tofrom.value, details.value, amount.valueAsNumber);
+    } else {
+        doc = new Payment(tofrom.value, details.value, amount.valueAsNumber);
+    }
+
+    console.log(doc);
 });
 
-const inv1 = new Invoice('ehsan', 'work on website', 300);
-const inv2 = new Invoice('james', 'work on website', 200);
+// let docOne: HasFormatter;
+// let docTwo: HasFormatter;
 
-let invoices: Invoice[] = [];
+// docOne = new Invoice('ehsan', 'web work', 250);
+// docTwo = new Invoice('james', 'teaching work', 200);
 
-invoices.push(inv1);
-invoices.push(inv2);
+// let docs: HasFormatter[] = [];
+// docs.push(docOne);
+// docs.push(docTwo);
 
-invoices.forEach(inv => {
-    console.log(inv.client, inv.amount, inv.format());
-});
+// const inv1 = new Invoice('ehsan', 'work on website', 300);
+// const inv2 = new Invoice('james', 'work on website', 200);
 
-interface IsPerson {
-    name: string;
-    age: number;
-    speak(a: string): void;
-    spend(a: number): number;
-}
+// let invoices: Invoice[] = [];
 
-const me: IsPerson = {
-    name: 'John',
-    age: 23,
-    speak(text: string): void {
-        console.log(text);
-    },
-    spend(amount: number): number {
-        console.log('I spent', amount);
-        return amount;
-    },
-};
+// invoices.push(inv1);
+// invoices.push(inv2);
 
-console.log(me);
+// invoices.forEach(inv => {
+//     console.log(inv.client, inv.amount, inv.format());
+// });
 
-const greeting = (person: IsPerson) => {
-    console.log(person.name);
-};
+// interface IsPerson {
+//     name: string;
+//     age: number;
+//     speak(a: string): void;
+//     spend(a: number): number;
+// }
 
-greeting(me);
+// const me: IsPerson = {
+//     name: 'John',
+//     age: 23,
+//     speak(text: string): void {
+//         console.log(text);
+//     },
+//     spend(amount: number): number {
+//         console.log('I spent', amount);
+//         return amount;
+//     },
+// };
+
+// console.log(me);
+
+// const greeting = (person: IsPerson) => {
+//     console.log(person.name);
+// };
+
+// greeting(me);
